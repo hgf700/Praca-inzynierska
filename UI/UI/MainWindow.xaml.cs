54eda0756880a3a16f8aec0e25a2f1052320ff36
hgf700 <148117806+hgf700@users.noreply.github.com>
2026-01-06 19:29:28 +0100

    started developing frontend

---
+++ UI/UI/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnClick1(object sender, RoutedEventArgs e)
+        {
+            btn1.IsEnabled = false;
+
+            await Task.Run(() =>
+            {
+                //RunGeneticAlgorithm();
+            });
+
+            btn1.IsEnabled = true;
+            MessageBox.Show("AG zakończony");
+        }
+    }
+}
